75d589b216446125532323cf4aa18d913ab6a606
Andrew-mie7 <63439849+Andrew-mie7@users.noreply.github.com>
2021-07-17 14:52:36 +0800

    Minor bugs

--- crontab_setup.sh
+++ crontab_setup.sh
@@ -8,9 +8,9 @@ sudo chmod 777 /media/$HIVE_ID/TOS
 
 MOUNT="@reboot root mount /dev/sda1 /media/$HIVE_ID/TOS"
 
-RECORD_DATE="*/1 * * * * root sh $(date) >> /home/$HIVE_ID/record.log"
+RECORD_DATE="*/1 * * * * root sh date >> /home/$HIVE_ID/record.log"
 RECORD="*/1 * * * * root sh /home/$HIVE_ID/Desktop/record.sh >> /home/$HIVE_ID/record.log 2>&1"
-MAIN_DATE="*/1 * * * * root sh $(date) >> /home/$HIVE_ID/main.log"
+MAIN_DATE="*/1 * * * * root sh date >> /home/$HIVE_ID/main.log"
 MAIN="*/1 * * * * $HIVE_ID python2 /home/$HIVE_ID/bee_sound/main.py >> /home/$HIVE_ID/main.log 2>&1"
 
 sudo echo "0 0 * * * root reboot" >> /etc/crontab
