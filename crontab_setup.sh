8c4f632e586a99ea90ab44a095952f7be2bc4ff2
Andrew-mie7 <63439849+Andrew-mie7@users.noreply.github.com>
2021-07-18 01:18:46 +0800

    Minor bug

--- crontab_setup.sh
+++ crontab_setup.sh
@@ -8,10 +8,10 @@ sudo chmod 777 /media/$HIVE_ID/TOS
 
 MOUNT="@reboot root mount /dev/sda1 /media/$HIVE_ID/TOS"
 RECORD_SH="sudo arecord -D plughw:2,0 -f S16_LE -d 60 -r 44100 --use-strftime /media/$HIVE_ID/TOS/sound/%Y%m%d-%H%M%v.wav"
-RECORD_DATE="*/1 * * * * $HIVE_ID date >> /home/$HIVE_ID/record.log"
+RECORD_DATE="*/1 * * * * $HIVE_ID date >> /home/$HIVE_ID/Desktop/record.log"
 RECORD="*/1 * * * * root sh /home/$HIVE_ID/Desktop/record.sh >> /home/$HIVE_ID/Desktop/record.log 2>&1"
 CHMOD_SOUND="*/1 * * * * root chmod -R 777 /media/$HIVE_ID/TOS/sound"
-MAIN_DATE="*/1 * * * * $HIVE_ID date >> /home/$HIVE_ID/main.log"
+MAIN_DATE="*/1 * * * * $HIVE_ID date >> /home/$HIVE_ID/Desktop/main.log"
 MAIN="*/1 * * * * $HIVE_ID python2 /home/$HIVE_ID/bee_sound/main.py >> /home/$HIVE_ID/Desktop/main.log 2>&1"
 
 sudo echo "$RECORD_SH" >> /home/$HIVE_ID/Desktop/record.sh
