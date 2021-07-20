ccfe8f1fdc66417711013215922ebcb5a9aa612f
yiying <s0o7m3e5@gmail.com>
2021-07-20 13:19:15 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ sudo apt install gfortran libatlas-base liblapack-dev
 sudo apt install numpy
 sudo apt install scipy
 sudo apt install python-sklearn python-sklearn-lib python-sklearn-doc
-sudo apt install pymysql
+sudo apt install python-mysql
 pip install schedule
 sh -x audio_setup.sh
-sh -x crontab_setup.sh
